87ca92ac919e399e968227cd2c9c5aa3a641454f
Ben <rath3.14159@protonmail.com>
2020-03-14 16:44:46 -0500

    Added Motion Detector

---
+++ Motion_Detector/Motion_Detector.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+class FrameMotionDetector():
+    def __init__(self, accumulated_weight):
+        self.accumulated_weight = accumulated_weight
+        self.background = None
+
+    def update(self, image):
+        if self.background is None:
+            self.background = image.copy().astype('float')
+            return
+        cv2.accumulateWeighted(image, self.background, self.accumulated_weight)
+
+    def detect(self, image, threshold_val):
+        delta = cv2.absdiff(self.background.astype("uint8"), image)
+        thresh = cv2.threshold(delta, threshold_val, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
+        contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) == 0:
+            return None
+        return (thresh)
